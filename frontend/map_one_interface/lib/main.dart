4655862836a647290f231a6facfb424c8ceaac02
Samantha Milligan <smm885@nau.edu>
2022-03-16 13:03:13 -0700

    updated frontend

--- frontend/map_one_interface/lib/main.dart
+++ frontend/map_one_interface/lib/main.dart
@@ -9,6 +9,8 @@ import 'package:map_one_interface/user.dart';
 import 'backendCalls.dart';
 import 'data.dart';
 import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'entry.dart';
 
 Icon SearchIcon = const Icon(Icons.search);
 Icon FaceIcon = const Icon(Icons.face);
@@ -16,114 +18,70 @@ Icon HomeIcon = const Icon(Icons.home);
 Widget Bar = const Text("Enter the query for the desired publication");
 // setting list to a string then spliting it into a new list on the main to call
 // overflows for some reason
- //String entryIdArr = backEndCalls(entryIdArr) as String;
+  // String entryIdArr = backEndCalls(entryIdArr) as String;
  //List EntryIdArr = entryIdArr.split(',');
 
 class backEndCalls extends MapOneHomePage
 {
-  List entryIdArr = [];
-  List SourceNameArr = [];
-  List SourceListArr = [];
-  List TitleArr = [];
-  List AuthorList = [];
-  List GlobalDataRowList = [];
-
-
-
   var globalPtr;
   String title = "MapOne";
   bool verboseFlag = true;
   //String inData;
 
+    backEndCalls(this.title) : super(title: title)
+    {
+      getApiEntries();
+    }
+
+    backEndCalls1()
+    {
+      getApiEntries();
+    }
 
 
-  backEndCalls(this.title) : super(title: title)
-  {
-    getApiEntries();
+
+    getApiEntries()
+    async
+    {
+      // get request
+      var response = await http.get(Uri.parse("https://mapone-api.herokuapp.com/entry/?action=0"));
+
+      // store date into list type
+      List<Entry> entries = new List<Entry>.from(json.decode(response.body).map(
+              (data) => Entry.fromJson(data)));
+
+      // declare all lists here -> source name, link, author, etc.
+      List body_list = [];
+
+      // add API data to lists
+      entries.forEach((element){body_list.add(element.map_body);});
+
+      // test
+      print(body_list);
+
+      // TODO: populate data table
+    }
   }
 
-  backEndCalls1()
+  //Function: populateDataRows
+  //approach: returns DataRow object with parameters as values
+  //link, body, scale, author, publicationData
+  populateDataRows()
   {
-    getApiEntries();
+    var dataR;
+    dataR = DataRow(
+        cells: <DataCell>[
+          DataCell(Text("Test")),
+          DataCell(Text("Test")),
+          DataCell(Text("Test")),
+          DataCell(Text("Test")),
+          DataCell(Text("Test")),
+          DataCell(Text("Test")),]
+    );
+
+    return dataR;
+
   }
-
-
-
-  getApiEntries()
-  async
-  {
-
-    Response publication = await HttpRequests.get("https://mapone-api.herokuapp.com/entry/?action=0");
-
-    var publicationContent = publication.response;
-
-
-    globalPtr = publicationContent;
-
-
-
-
-    jsonDecode(publicationContent);
-
-    serializeApiEntries(publicationContent);
-  }
-
-  serializeApiEntries(pubcontent)
-  {
-
-    pubcontent = globalPtr;
-
-    String responseStr = pubcontent;
-
-    int i= 0;
-    entryIdArr.add(pubcontent);
-    print(entryIdArr.elementAt(0));
-
-// comment out if no work
-    // while(i < responseStr.length)
-    //    {
-    //      if(responseStr[i] == "{")
-    //       {
-    //       print(responseStr[i+12]);
-    //     entryIdArr.add(responseStr[i+12]);
-    //   print(responseStr.substring(i+28, i+38));
-    //     print(responseStr.substring(i+125, i+140));
-    //     SourceNameArr.add(responseStr.substring(i+28, i+38));
-    //    SourceListArr.add(responseStr.substring(i+54, i+120));
-    //   TitleArr.add(responseStr.substring(i+140, i+200));
-
-    //}
-    //i++;
-  }
-
-
-
-//  }
-//populateDataTable(entryIdArr, SourceNameArr, SourceListArr, TitleArr);
-
-
-
-}
-//comment out if no work
-//Function: populateDataRows
-//approach: returns DataRow object with parameters as values
-//link, body, scale, author, publicationData
-populateDataRows()
-{
-  var dataR;
-  dataR = DataRow(
-      cells: <DataCell>[
-        DataCell(Text("")),
-        DataCell(Text("")),
-        DataCell(Text("")),
-        DataCell(Text("")),
-        DataCell(Text("")),
-        DataCell(Text("")),]
-  );
-
-  return dataR;
-
-}
 
 void main() {
 
@@ -167,7 +125,7 @@ class MapOneHomePage extends StatefulWidget {
   class _MapOneHomePageState extends State<MapOneHomePage> {
 
     backEndCalls  backendObjk = new backEndCalls("Map One Alpha");
-    List <dynamic>  backendObj = new backEndCalls("Map One Alpha").entryIdArr;
+    //List <dynamic>  backendObj = new backEndCalls("Map One Alpha").entryIdArr;
 
     //backendObj;
 
