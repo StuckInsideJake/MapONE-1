367445b5befff9e88bb92d255d89466eb351529e
StuckInsideJake <66990588+StuckInsideJake@users.noreply.github.com>
2022-03-22 11:49:12 -0700

    SF DataGrid working

--- frontend/map_one_interface/lib/main.dart
+++ frontend/map_one_interface/lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:async_builder/async_builder.dart';
 import 'package:http_requests/http_requests.dart';
@@ -11,6 +13,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'entry.dart';
 import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+import "package:intl/intl.dart";
 
 Icon SearchIcon = const Icon(Icons.search);
 Icon FaceIcon = const Icon(Icons.face);
@@ -22,8 +25,24 @@ Widget Bar = const Text("Enter the query for the desired publication");
   // String entryIdArr = backEndCalls(entryIdArr) as String;
  //List EntryIdArr = entryIdArr.split(',');
 
+    Future<EntryDataGridSource> getEntryDataSource() async{
+      var entryList = await getApiEntries();
+      return EntryDataGridSource(entryList);
 
-
+    }
+    List<GridColumn> getColumns()
+      {
+       return <GridColumn>[
+         GridTextColumn(columnName: "Entry ID",
+         width: 70,
+         label: Container(
+           padding: EdgeInsets.all(8),
+           alignment: Alignment.centerLeft,
+           child: Text("Entry ID",
+           overflow: TextOverflow.clip, softWrap: true )
+         ),
+         )];
+      }
     Future getApiEntries()
     async
     {
@@ -40,7 +59,6 @@ Widget Bar = const Text("Enter the query for the desired publication");
 
     }
 
-
    class EntryDataGridSource extends DataGridSource
       {
         EntryDataGridSource(this.entryList)
@@ -50,13 +68,12 @@ Widget Bar = const Text("Enter the query for the desired publication");
         late List<DataGridRow> dataGridRows;
         late List<Entry> entryList;
 
-
         @override
         DataGridRowAdapter? buildRow(DataGridRow row)
          {
            return DataGridRowAdapter(cells: [
              Container(
-               child: Text(row.getCells()[0].value.toString();
+               child: Text(row.getCells()[0].value.toString(),
                overflow: TextOverflow.ellipsis,
                ),
                alignment: Alignment.centerLeft,
@@ -77,15 +94,12 @@ Widget Bar = const Text("Enter the query for the desired publication");
 
       }
 
-
-
   //Function: populateDataRows
   //approach: returns DataRow object with parameters as values
   //link, body, scale, author, publicationData
   populateDataRow()
   {
     var dataR;
-
 
     dataR = DataRow(
         cells: <DataCell>[
@@ -143,141 +157,17 @@ class MapOneHomePage extends StatefulWidget {
 
   class _MapOneHomePageState extends State<MapOneHomePage> {
 
-
-
-
-
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Bar,
-        automaticallyImplyLeading: false,
-        actions: [
-          IconButton(
-            onPressed: () {
-              setState(() {
+  Widget build(BuildContext context)
+     {
+      return SafeArea(child: Scaffold(body: FutureBuilder(
+        future: getEntryDataSource(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot){
+          return snapshot.hasData
+              ? SfDataGrid(source: snapshot.data, columns: getColumns())
+              : Center(child: CircularProgressIndicator(strokeWidth: 3,) );
+        },
+      ),),);
+     }
 
-                if(SearchIcon.icon == Icons.search)
-                {
-                  SearchIcon = const Icon(Icons.cancel);
-                  Bar = const ListTile(
-                      leading: Icon(
-                          Icons.search,
-                          color: Colors.white,
-                          size: 28));
-                  title: TextField( decoration: InputDecoration(
-                      hintText: 'type in journal name...',
-                      hintStyle: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontStyle: FontStyle.italic)));
-                  border: InputBorder.none;
-                  style: TextStyle(color: Colors.grey);
-                }
-                else
-                {
-                  SearchIcon = const Icon(Icons.search);
-                  Bar = const Text("Enter your query");
-                }
-              });
-            },
-            icon: SearchIcon,
-          ),
-          IconButton(
-            onPressed:
-          ()
-            {
-              // in order to change view, first the current
-              // rendered context must be popped and then the
-              // new one must be pushed onto the build stack
-              Navigator.pop(context);
-              Navigator.push(context, MaterialPageRoute(builder:
-                  (context) => user()));
-            },
-            icon: FaceIcon,
-            ),
-          IconButton(
-            onPressed:
-                ()
-            {
-              // in order to change view, first the current
-              // rendered context must be popped and then the
-              // new one must be pushed onto the build stack
-              Navigator.pop(context);
-              Navigator.push(context, MaterialPageRoute(builder:
-                  (context) => MapOne()));
-            },
-            icon:HomeIcon,
-          )
-        ], // Actions
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            DataTable(
-              columns: const <DataColumn>[
-                DataColumn(
-                  label: Text("Entry",
-                  style: TextStyle(fontStyle: FontStyle.italic),
-
-                  ),
-                ),
-                DataColumn(
-                  label: Text("Link",
-                  style: TextStyle(fontStyle: FontStyle.italic),
-
-                ),
-                ),
-                DataColumn(
-                  label: Text("Body",
-                    style: TextStyle(fontStyle: FontStyle.italic),
-                  ),
-                ),
-                DataColumn(
-                  label: Text("",
-                    style: TextStyle(fontStyle: FontStyle.italic),
-                  ),
-                ),
-                DataColumn(
-                  label: Text("Author",
-                    style: TextStyle(fontStyle: FontStyle.italic),
-                  ),
-                ),
-                DataColumn(
-                  label: Text("Publisher",
-                    style: TextStyle(fontStyle: FontStyle.italic),
-                  ),
-                ),
-              ],
-              rows:  <DataRow>
-                  [
-                    populateDataRow()
-
-                  ],
-            ),
-
-            Card(
-                margin: EdgeInsets.symmetric(horizontal: 20.0,vertical: 8.0),
-                elevation: 2.0,
-                child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 12,horizontal: 30),
-                    child: Text(" ",style: TextStyle(
-                        letterSpacing: 2.0,
-                        fontWeight: FontWeight.w300
-                    ),))
-            ),
-
-            ]
-          //],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        tooltip: 'This page allows the user to save ',
-        child: Icon(Icons.info),
-      ),
-    );}
 }
